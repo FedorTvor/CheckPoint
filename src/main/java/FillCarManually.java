eb61c55177ef7c22c8bedccf0f6aca951adfe3ad
MrDokter <7397a7397@gmail.com>
2025-02-09 18:24:46 +0300

    fix fill manual for car and add two help-class

--- src/main/java/FillCarManually.java
+++ src/main/java/FillCarManually.java
@@ -1,42 +1,32 @@
 import java.util.Scanner;
 
-public class FillCarCarManually implements FillCar {
+public class FillCarManually implements FillCar {
 
     @Override
     public Car[] fill() {
         Scanner in = new Scanner(System.in);
 
         System.out.println("Ведите число машин: ");
+        Integer countCar = InputHelp.InputInt(in);
 
-        int countCar = 0;
+        Car[] cars = null;
 
-        if (in.hasNextInt()) {
-            countCar = in.nextInt();
-        }
-
-        Car[] cars = new Car[countCar];
+        if(countCar != null && countCar > 0) {
+            cars = new Car[countCar];
+        } else countCar = 0;
 
         for(int i = 0; i < countCar; i++) {
 
-            in.nextLine();
-
             System.out.println("Введите мошность: ");
-            Integer power = null;
-            if (in.hasNextInt()) {
-                power = in.nextInt();
-                in.nextLine();
-            }
+            Integer power = InputHelp.InputInt(in);
 
             System.out.println("Введите модель: ");
             String model = in.nextLine();
 
             System.out.println("Введите год выпуска: ");
-            Integer year = null;
-            if (in.hasNextInt()) {
-                year = in.nextInt();
-            }
+            Integer year = InputHelp.InputInt(in);
 
-            if(power == null || year == null || model.equals("") || year < 1000 || year > 10000 || power < 0) {
+            if (!CheckHelp.carCheck(power, model, year)) {
                 System.out.println("Некоректно введённые данные");
                 i--;
             } else {
@@ -47,7 +37,6 @@ public class FillCarCarManually implements FillCar {
                         .build();
             }
         }
-
         return cars;
     }
 }
