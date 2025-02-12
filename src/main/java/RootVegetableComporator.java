1e63af06777389964ebd3a3a61ac016e0f5f432c
Evgeniya <jane_wild_cat@mail.ru>
2025-02-13 00:54:04 +0300

    fix for previous version of dopSort

--- src/main/java/RootVegetableComporator.java
+++ src/main/java/RootVegetableComporator.java
@@ -1,12 +1,12 @@
-//public class RootVegetableComporator implements CustomComparator<RootVegetableComporator>{
-//	@Override
-//	public int compare(RootVegetableComporator rootVgtbl1, RootVegetableComporator rootVgtbl2) {
-//		if (!rootVgtbl1.equals(rootVgtbl2) {
-//			return CustomComparator.compareINT(rootVgtbl1, rootVgtbl2);
-//		} else if (!rootVgtbl1.equals(rootVgtbl2)) {
-//			return rootVgtbl1.compareTo(rootVgtbl2);
-//		} else {
-//			return CustomComparator.compareINT(rootVgtbl1, rootVgtbl2);
-//		}
-//	};
-//	}
+public class RootVegetableComporator implements CustomComparator<RootVegetable>{
+    @Override
+    public int compare(RootVegetable rootVgtbl1, RootVegetable rootVgtbl2) {
+        if (rootVgtbl1.getWeight() != rootVgtbl2.getWeight()) {
+            return CustomComparator.compareINT(rootVgtbl1.getWeight(), rootVgtbl2.getWeight());
+        } else if (!rootVgtbl1.getType().equals(rootVgtbl2.getType())) {
+            return rootVgtbl1.getType().compareTo(rootVgtbl2.getType());
+        } else {
+            return rootVgtbl1.getColour().compareTo(rootVgtbl2.getColour());
+        }
+    };
+}
