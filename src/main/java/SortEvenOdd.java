397055e69a4e695b4edec906a1c735aa7176ce81
fedor tvorogov <fedor_tvorogov@mail.ru>
2025-02-12 17:29:32 +0300

    upd SortEvenOdd

--- src/main/java/SortEvenOdd.java
+++ src/main/java/SortEvenOdd.java
@@ -4,6 +4,7 @@ import java.util.function.Function;
 public class SortEvenOdd <T> {
 
 	private final CustomComparator<T> comparator;
+
 	public SortEvenOdd(CustomComparator<T> comparator) {
 		this.comparator = comparator;
 	}
@@ -13,31 +14,30 @@ public class SortEvenOdd <T> {
 			return array;
 		}
 		int evenCount = 0;
-		for (T arr: array) {
+		for (T arr : array) {
 			if (extractor.apply(arr) % 2 == 0) {
 				evenCount++;
 			}
 		}
-		T[] evenMas = (T[]) Array.newInstance(array.getClass().getComponentType(),evenCount);
-		T[] notEvenMas = (T[]) Array.newInstance(array.getClass().getComponentType(),array.length - evenCount);
+		T[] evenMas = (T[]) Array.newInstance(array.getClass().getComponentType(), evenCount);
 		int evenStep = 0;
-		int notEvenStep = 0;
 		for (T arr : array) {
 			if (extractor.apply(arr) % 2 == 0) {
-				evenMas[evenStep++] = arr;
-			} else {
-				notEvenMas[notEvenStep++] = arr;
+				evenMas[evenStep] = arr;
+				evenStep++;
 			}
 		}
-		T[] sortedEvenArray =(T[]) new Sort<>(comparator).mergeSort(evenMas);
+		T[] sortedEvenArray = (T[]) new Sort<>(comparator).mergeSort(evenMas);
 		T[] resultArray = (T[]) Array.newInstance(array.getClass().getComponentType(), array.length);
 		evenStep = 0;
-		notEvenStep = 0;
 		for (int i = 0; i < resultArray.length; i++) {
 			if (extractor.apply(array[i]) % 2 == 0) {
-				resultArray[i] = sortedEvenArray[evenStep++];
+				resultArray[i] = sortedEvenArray[evenStep];
+				evenStep++;
+				System.out.println(resultArray[i] + "+");
 			} else {
-				resultArray[i] = notEvenMas[notEvenStep++];
+				resultArray[i] = array[i];
+				System.out.println(resultArray[i] + "-");
 			}
 		}
 		return resultArray;
