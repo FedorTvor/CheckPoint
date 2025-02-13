de2926fd6e1305a0261e4fb1270ee3a6103202eb
Ivan Farlenkov <yo_99@mail.ru>
2025-02-13 22:36:54 +0500

    Поправил название переменной

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,7 +18,7 @@ public class Main {
         print_array_menu.addAction("3", "Корнепллод",new PrintArrayAction<RootVegetable>(datatype.ROOTVEGETABLE));
         print_array_menu.addAction("0", "Отмена", null);
         
-        Menu sort_menu=new Menu();
+        Menu search_menu=new Menu();
         
         CustomComparator<Integer> IntComp=(Integer a, Integer b)->(b-a);
         CustomComparator<String> StrComp=(String a, String b)->(a.compareTo(b));
@@ -26,8 +26,8 @@ public class Main {
         Function<String,Integer> Str2Int=(String str) ->(Integer.parseInt(str));
         Function<String,String> Str2Str=(String str) ->(str);
         
-        sort_menu.prelude="Выберите тип данных";
-        sort_menu.addAction("1", "Автомобиль",new Menu().
+        search_menu.prelude="Выберите тип данных";
+        search_menu.addAction("1", "Автомобиль",new Menu().
             addAction("1", "По году", new SearchAction().
                 setGetter(new YearOfCar()).
                 setComp(IntComp).
@@ -43,7 +43,7 @@ public class Main {
                 setParser(Str2Str).setSelector(datatype.CAR)
             ).addAction("0", "Отмена", null)
         );
-        sort_menu.addAction("2", "Книга",new Menu().
+        search_menu.addAction("2", "Книга",new Menu().
             addAction("1", "По названию", new SearchAction()
                 .setGetter(new TitleOfBook())
                 .setComp(StrComp)
@@ -58,7 +58,7 @@ public class Main {
                 setParser(Str2Int).setSelector(datatype.BOOK)
             ).addAction("0", "Отмена", null)
         );
-        sort_menu.addAction("3", "Корнепллод", new Menu().
+        search_menu.addAction("3", "Корнепллод", new Menu().
             addAction("1", "По типу", new SearchAction().
                 setGetter(new TypeOfRV()).
                 setComp(StrComp).
@@ -73,7 +73,7 @@ public class Main {
                 setParser(Str2Int).setSelector(datatype.ROOTVEGETABLE)
             ).addAction("0", "Отмена", null)
         );
-        sort_menu.addAction("0", "Отмена", null);
+        search_menu.addAction("0", "Отмена", null);
         
         Menu menu = new Menu();
         menu.reopen_after_submenu_closes=true;
@@ -81,7 +81,7 @@ public class Main {
         menu.addAction("2", "Создание массива объектов с рандомными значениями", new RandomFillArrayAction());
         menu.addAction("3", "Вывод массива объектов на экран", print_array_menu);
         menu.addAction("4", "Сортировка массива", null);//TODO
-        menu.addAction("5", "Поиск Объекта по значению", sort_menu);
+        menu.addAction("5", "Поиск объекта по значению", search_menu);
         menu.addAction("0", "Выход", null);
 
         menu.execute();
