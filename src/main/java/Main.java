78fb3d7d7777d8a19340c49be1ea8f26551230d5
Nickolai Chernyshev <nickchern2013@gmail.com>
2025-02-13 17:31:07 +0300

    реализованна mergeSort

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,14 +16,21 @@ public class Main {
         print_array_menu.addAction("2", "Книга",new PrintArrayAction<Book>(datatype.BOOK));
         print_array_menu.addAction("3", "Корнепллод",new PrintArrayAction<RootVegetable>(datatype.ROOTVEGETABLE));
         print_array_menu.addAction("0", "Отмена", null);
+
+        Menu sort_array_menu=new Menu();
+        sort_array_menu.prelude="Выберите тип данных";
+        sort_array_menu.addAction("1", "Автомобиль",new SortAction<Car>(datatype.CAR));
+        sort_array_menu.addAction("2", "Книга",new SortAction<Book>(datatype.BOOK));
+        sort_array_menu.addAction("3", "Корнепллод",new SortAction<RootVegetable>(datatype.ROOTVEGETABLE));
+        sort_array_menu.addAction("0", "Отмена", null);
         
         Menu menu = new Menu();
         menu.reopen_after_submenu_closes=true;
-        menu.addAction("1", "Заполнение массива объектов из файла", new ManuallyFillArrayAction());
+        menu.addAction("1", "Заполнение массива объектов из файла", null);
         menu.addAction("2", "Заполнение массива объектов вручную", new ManuallyFillArrayAction());
         menu.addAction("3", "Заполнение массива объектов рандомными значениями", new RandomFillArrayAction());
         menu.addAction("4", "Вывод массива объектов на экран", print_array_menu);
-        menu.addAction("5", "Сортировка массива", null);//TODO
+        menu.addAction("5", "Сортировка массива", sort_array_menu);//TODO
         menu.addAction("6", "Поиск элемента в массиве", new SearchAction());
         menu.addAction("0", "Выход", new ExitAction());
 
--- src/main/java/ManuallyFillArrayAction.java
+++ src/main/java/ManuallyFillArrayAction.java
@@ -1,8 +1,80 @@
 import java.util.Scanner;
 
 public class ManuallyFillArrayAction implements MenuAction{
+
     @Override
     public void execute(){
+        Scanner scanner = new Scanner(System.in);
+        boolean step = true;
 
+        while (step){
+            System.out.println("Выберите тип данных для массива:");
+            System.out.println("1. Автомобиль");
+            System.out.println("2. Книга");
+            System.out.println("3. Корнепллод");
+            System.out.println("0. Вернуться в главное меню");
+
+            String choice = scanner.next();
+            switch (choice) {
+                case "1":
+                    do{// Ввод автомобилей вручную
+                    FillCarManually fillCarManually = new FillCarManually();
+                    Car[] cars = fillCarManually.fill();
+
+                    Storage<Car> storage= Storage.<Car>getInstance(Main.datatype.CAR);
+                    int start_index=findFirst(storage,cars.length);
+                    Object[] objects = storage.getObjects();
+
+                    System.arraycopy(cars, 0, objects, start_index,cars.length);
+                    step = false;
+                    }while(false);
+                    break;
+                case "2":
+                    do{// Ввод книг вручную
+                    FillBookManually fillBookManually = new FillBookManually();
+                    Book[] books = fillBookManually.fill();
+
+                    Storage<Book> storage= Storage.<Book>getInstance(Main.datatype.BOOK);
+                    int start_index=findFirst(storage,books.length);
+                    Object[] objects = storage.getObjects();
+
+                    System.arraycopy(books, 0, objects, start_index,books.length);
+                    step = false;
+                    }while(false);
+                    break;
+                case "3":
+                    do{// Ввод корнеподов вручную
+                    FillRootVegetableManually fillRootVegetableManually = new FillRootVegetableManually();
+                    RootVegetable[] vegetables = fillRootVegetableManually.fill();
+
+                    Storage<RootVegetable> storage= Storage.<RootVegetable>getInstance(Main.datatype.ROOTVEGETABLE);
+                    int start_index=findFirst(storage,vegetables.length);
+                    Object[] objects = storage.getObjects();
+
+                    System.arraycopy(vegetables, 0, objects, start_index,vegetables.length);
+                    step = false;
+                    }while(false);
+
+                    break;
+                case "0":
+                    step = false;
+                    break;
+            }
+        }
+    }
+
+    private int findFirst(Storage<? extends Object>storage, int size){
+        Object[] objects=storage.getObjects();
+        int startIndex = 0;
+        // Найти первое свободное место в массиве
+        while (startIndex < objects.length && objects[startIndex] != null) {
+            startIndex++;
+        }
+        // Если места нет увеличиваем размер массива
+        if (startIndex >= objects.length) {
+            storage.expandArray(size);
+            objects = storage.getObjects();
+        }
+        return startIndex;
     }
 }
