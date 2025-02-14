1ba434d06919eef85d916e3939fa5e80479b0b63
Ivan Farlenkov <yo_99@mail.ru>
2025-02-14 15:22:33 +0500

    Починил класс EvenOddAction

--- src/main/java/EvenOddAction.java
+++ src/main/java/EvenOddAction.java
@@ -1,11 +1,28 @@
-class EvenOddAction implements MenuAction{
-    public Main.datatype class_selector;
+import java.util.function.Function;
+
+class EvenOddAction<T> implements MenuAction{
+    private Main.datatype class_selector;
     private CustomComparator<T> comparator;
     private Function<T, Integer> extractor;
     
-    private void execute(){
-        if(comporator==null || extractor ==null |class_selector==null){
-            throw new Exception("Заполни поля");
+    public EvenOddAction setSelector(Main.datatype i){
+        class_selector=i;
+        return this;
+    }
+    
+    public EvenOddAction setComp(CustomComparator<T> comparator){
+        this.comparator=comparator;
+        return this;
+    }
+    
+    public EvenOddAction setExtractor(Function<T, Integer> extractor){
+        this.extractor=extractor;
+        return this;
+    }
+    
+    public void execute(){
+        if(comparator==null || extractor ==null |class_selector==null){
+            System.out.println("Заполни поля");
         }
         Storage<T> storage= Storage.<T>getInstance(class_selector);
         SortEvenOdd <T> sort= new SortEvenOdd <T>(comparator);
